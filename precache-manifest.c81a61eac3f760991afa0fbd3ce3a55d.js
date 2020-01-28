b9e08e201188460419783b893646016d2e4b4d89
Ridhwan <ridhwan.razali@myunixus.com>
2020-01-28 15:10:15 +0800

    Updates

---
+++ precache-manifest.c81a61eac3f760991afa0fbd3ce3a55d.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "4692776ef34cd9adcfa1",
+    "url": "/scdswebsite/static/css/main.59169dfe.chunk.css"
+  },
+  {
+    "revision": "4692776ef34cd9adcfa1",
+    "url": "/scdswebsite/static/js/main.6c164aa8.chunk.js"
+  },
+  {
+    "revision": "1d15376147e0cc0fb4f5",
+    "url": "/scdswebsite/static/js/runtime~main.2c4ab673.js"
+  },
+  {
+    "revision": "cc76a77edb3f45b30317",
+    "url": "/scdswebsite/static/js/2.b7ac2738.chunk.js"
+  },
+  {
+    "revision": "948aa7955600c822d2a475c2c55ee3ab",
+    "url": "/scdswebsite/static/media/ccis.948aa795.png"
+  },
+  {
+    "revision": "64dceae77a59bd27b67a9148e55ff1b8",
+    "url": "/scdswebsite/static/media/SCDS2020.64dceae7.jpg"
+  },
+  {
+    "revision": "6bca8c63ee39574b1534650c425c0acb",
+    "url": "/scdswebsite/static/media/book1.6bca8c63.jpg"
+  },
+  {
+    "revision": "5b446608252a9552db7a76122dee0550",
+    "url": "/scdswebsite/static/media/book2.5b446608.png"
+  },
+  {
+    "revision": "9f0dcb93246df63c9d6c32816a7fe49a",
+    "url": "/scdswebsite/static/media/book3.9f0dcb93.jpg"
+  },
+  {
+    "revision": "cd607090b5c14fb1eda78189f71405c9",
+    "url": "/scdswebsite/static/media/book4.cd607090.png"
+  },
+  {
+    "revision": "68f3a649d4ee8f524295d3813ac4d377",
+    "url": "/scdswebsite/static/media/speaker1.68f3a649.png"
+  },
+  {
+    "revision": "7cfb3d0e08c4ff966130945b1ada7686",
+    "url": "/scdswebsite/static/media/speaker2.7cfb3d0e.png"
+  },
+  {
+    "revision": "dca3101b646b6fd00a494d9f49c770b9",
+    "url": "/scdswebsite/static/media/speaker3.dca3101b.png"
+  },
+  {
+    "revision": "d345eb855d5e55c4f494da21c3b4aae5",
+    "url": "/scdswebsite/static/media/speaker4.d345eb85.png"
+  },
+  {
+    "revision": "e65a49b931fbbd1f3a89c858007238c3",
+    "url": "/scdswebsite/static/media/banner-bg.e65a49b9.jpg"
+  },
+  {
+    "revision": "fd54476f46e0bb52d52a25b194c86f9c",
+    "url": "/scdswebsite/static/media/fee.fd54476f.png"
+  },
+  {
+    "revision": "7d961a598d905d6bb6b377a90e99131b",
+    "url": "/scdswebsite/static/media/scds2015.7d961a59.jpg"
+  },
+  {
+    "revision": "94cee068557fc25d42d01e4b5b91dacf",
+    "url": "/scdswebsite/static/media/scds2016.94cee068.gif"
+  },
+  {
+    "revision": "e644876e81a9bbbeb4efc00cfb6dbdc9",
+    "url": "/scdswebsite/static/media/scds2017.e644876e.jpg"
+  },
+  {
+    "revision": "735957ed34f0934a8b2eb9ab1653a080",
+    "url": "/scdswebsite/static/media/scds2018.735957ed.jpg"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/scdswebsite/static/media/boatImage.d61546cd.jpg"
+  },
+  {
+    "revision": "ef325b22f4a1ee231ae4ecd4eee5c19d",
+    "url": "/scdswebsite/index.html"
+  }
+];
