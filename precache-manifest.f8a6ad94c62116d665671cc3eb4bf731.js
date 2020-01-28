f7c89859e31cbff03527823e8fb8fe031af53a6f
Ridhwan <ridhwan.razali@myunixus.com>
2020-01-28 17:33:40 +0800

    Updates

---
+++ precache-manifest.f8a6ad94c62116d665671cc3eb4bf731.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "32f20e0e6ad83eec5561",
+    "url": "/scds/static/css/main.59169dfe.chunk.css"
+  },
+  {
+    "revision": "32f20e0e6ad83eec5561",
+    "url": "/scds/static/js/main.5bef0559.chunk.js"
+  },
+  {
+    "revision": "49ab06b2ee916d0524ea",
+    "url": "/scds/static/js/runtime~main.aaf6aafd.js"
+  },
+  {
+    "revision": "f12caa72b405facb9e9a",
+    "url": "/scds/static/js/2.e2482be0.chunk.js"
+  },
+  {
+    "revision": "948aa7955600c822d2a475c2c55ee3ab",
+    "url": "/scds/static/media/ccis.948aa795.png"
+  },
+  {
+    "revision": "64dceae77a59bd27b67a9148e55ff1b8",
+    "url": "/scds/static/media/SCDS2020.64dceae7.jpg"
+  },
+  {
+    "revision": "6bca8c63ee39574b1534650c425c0acb",
+    "url": "/scds/static/media/book1.6bca8c63.jpg"
+  },
+  {
+    "revision": "5b446608252a9552db7a76122dee0550",
+    "url": "/scds/static/media/book2.5b446608.png"
+  },
+  {
+    "revision": "9f0dcb93246df63c9d6c32816a7fe49a",
+    "url": "/scds/static/media/book3.9f0dcb93.jpg"
+  },
+  {
+    "revision": "cd607090b5c14fb1eda78189f71405c9",
+    "url": "/scds/static/media/book4.cd607090.png"
+  },
+  {
+    "revision": "68f3a649d4ee8f524295d3813ac4d377",
+    "url": "/scds/static/media/speaker1.68f3a649.png"
+  },
+  {
+    "revision": "7cfb3d0e08c4ff966130945b1ada7686",
+    "url": "/scds/static/media/speaker2.7cfb3d0e.png"
+  },
+  {
+    "revision": "dca3101b646b6fd00a494d9f49c770b9",
+    "url": "/scds/static/media/speaker3.dca3101b.png"
+  },
+  {
+    "revision": "d345eb855d5e55c4f494da21c3b4aae5",
+    "url": "/scds/static/media/speaker4.d345eb85.png"
+  },
+  {
+    "revision": "e65a49b931fbbd1f3a89c858007238c3",
+    "url": "/scds/static/media/banner-bg.e65a49b9.jpg"
+  },
+  {
+    "revision": "fd54476f46e0bb52d52a25b194c86f9c",
+    "url": "/scds/static/media/fee.fd54476f.png"
+  },
+  {
+    "revision": "7d961a598d905d6bb6b377a90e99131b",
+    "url": "/scds/static/media/scds2015.7d961a59.jpg"
+  },
+  {
+    "revision": "94cee068557fc25d42d01e4b5b91dacf",
+    "url": "/scds/static/media/scds2016.94cee068.gif"
+  },
+  {
+    "revision": "e644876e81a9bbbeb4efc00cfb6dbdc9",
+    "url": "/scds/static/media/scds2017.e644876e.jpg"
+  },
+  {
+    "revision": "735957ed34f0934a8b2eb9ab1653a080",
+    "url": "/scds/static/media/scds2018.735957ed.jpg"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/scds/static/media/boatImage.d61546cd.jpg"
+  },
+  {
+    "revision": "ef762d2f44d5aff7264fb0cbd5ed02d9",
+    "url": "/scds/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e27cbe4a2da8952ece6c098f19cc14e7.js"
+  "/scds/precache-manifest.f8a6ad94c62116d665671cc3eb4bf731.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/scds/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
